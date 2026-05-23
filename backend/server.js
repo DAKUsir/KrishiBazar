8511953a685e579de9473fd4e0c3ef3d66aff386
DAKUsir <chauhanaditya6113@gmail.com>
2026-05-23 07:58:07 +0530

    ip

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 require('dotenv').config({ path: '../.env' });
+const dns = require('dns');
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
 const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
